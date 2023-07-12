590966f8438d9aae3c0b6ddf7dc04926e598424f
Artur <asady502@gmail.com>
2023-07-12 18:15:40 +0200

    add html additem.html          cartView.html          summary.html add ModelAndView

--- src/main/java/com/artursady/MyShop/controller/HomeController.java
+++ src/main/java/com/artursady/MyShop/controller/HomeController.java
@@ -1,14 +1,23 @@
 package com.artursady.MyShop.controller;
 
+
+import com.artursady.MyShop.model.Item;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Controller
 public class HomeController {
+    private List<Item> items = List.of(new Item("Ołówek", new BigDecimal("1.50"), "https://dlabiura24.pl/i/cms/000484.jpg"),
+            new Item("Długopis", new BigDecimal("2.10"), "https://ebiurowe.net/environment/cache/images/900_900_productGfx_29494/8ec091acb44978de25d21ba751fda65e.jpg"),
+            new Item("Pióro", new BigDecimal("9.50"), "https://www.luxuryproducts.pl/file/image/w800/h800/fit233568i0ad9c8dd139477229b600056c28249f47201526.jpg"));
 
-@GetMapping("/")
-    public String home(){
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("items", items);
         return "home";
 
     }
